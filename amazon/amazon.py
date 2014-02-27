76dcdf60793b0f4c8b90e2091b7db7097516106b
Virantha Ekanayake <virantha@gmail.com>
2014-02-27 14:41:21 -0500

    Added my amazon affiliate linker

---
+++ amazon/amazon.py
@@ -0,0 +1,42 @@
+from __future__ import unicode_literals
+
+from docutils import nodes
+from docutils.parsers.rst import directives, Directive
+from logging import error
+
+from pelican import signals
+import re
+
+class Amazon(Directive):
+    required_arguments = 3
+    has_content= False
+    final_argument_whitespace= True
+
+    def run(self):
+        error_text = "Amazon directive: "
+        params = {}
+        params['asin'] = self.arguments[0].strip()
+        params['link_type'] = self.arguments[1].strip()
+        if params['link_type'] not in ['image', 'text']: 
+            error(error_text + '2nd argument must be image or text')
+
+        params['text'] = self.arguments[2].strip()
+
+        link_type = params['link_type']
+        if link_type == 'text':
+            myhtml = '<a href="http://www.amazon.com/gp/product/%(asin)s?ie=UTF8&linkCode=as2&camp=1634&tag=virantha-20">%(text)s</a>' % (params)
+        elif link_type == 'image':
+            myhtml = '<div><a href="http://www.amazon.com/gp/offer-listing/%(asin)s/ref=as_li_tf_il?ie=UTF8&camp=1789&creative=9325&creativeASIN=%(asin)s&linkCode=am2&tag=virantha-20&"><img border="0" src="http://ws-na.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=%(asin)s&Format=_SL110_&ID=AsinImage&MarketPlace=US&ServiceVersion=20070822&WS=1&tag=virantha-20" style="vertical-align: middle;"/>%(text)s</a></div>' % params
+
+        return [nodes.raw('', myhtml, format='html')]
+
+def parse_for_amazon(article_generator):
+    for article in article_generator.articles:
+        mAmzn = re.compile(r"""\[amazon (?P<asin>[\w\d]+) (?P<text>[\w\d\s]+)\]""")
+        myhtml = '<a href="http://www.amazon.com/gp/product/\g<asin>?ie=UTF8&linkCode=as2&camp=1634&tag=virantha-20">\g<text></a>'
+        article._content = mAmzn.sub(myhtml, article._content)
+
+
+def register():
+    directives.register_directive('amazon', Amazon)
+    signals.article_generator_finalized.connect(parse_for_amazon)
